e5cc10722f46b2008a7cde68b67e7af7be741c3c
Poonam1708 <51155609+Poonam1708@users.noreply.github.com>
2019-05-29 16:22:54 +0530

    Update CustomerOrderHistoryController.java

--- order-history-view-service/src/main/java/io/eventuate/examples/tram/ordersandcustomers/orderhistoryviewservice/web/customers/CustomerOrderHistoryController.java
+++ order-history-view-service/src/main/java/io/eventuate/examples/tram/ordersandcustomers/orderhistoryviewservice/web/customers/CustomerOrderHistoryController.java
@@ -1,7 +1,5 @@
 package io.eventuate.examples.tram.ordersandcustomers.orderhistoryviewservice.web.customers;
 
-import io.eventuate.examples.tram.ordersandcustomers.orderhistory.common.CustomerView;
-import io.eventuate.examples.tram.ordersandcustomers.orderhistory.backend.CustomerViewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,21 +11,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CustomerOrderHistoryController {
 
-  private final CustomerViewRepository customerViewRepository;
+  // private CustomerService customerService;
+  private CustomerRepository customerViewRepository;
 
   @Autowired
-  public CustomerOrderHistoryController(CustomerViewRepository customerViewRepository) {
+  public CustomerOrderHistoryController(CustomerRepository customerViewRepository) {
+    //this.customerService = customerService;
     this.customerViewRepository = customerViewRepository;
   }
+ 
 
   @RequestMapping(value="/customers/{customerId}", method= RequestMethod.GET)
-  public ResponseEntity<CustomerView> getCustomer(@PathVariable Long customerId) {
-    CustomerView customer = customerViewRepository.findOne(customerId);
-    System.out.println("Found customer=" + customer + " for " + customerId);
-    if (customer == null)
-      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    else
-      return new ResponseEntity<>(customer, HttpStatus.OK);
+  public ResponseEntity<GetCustomerResponse> getCustomer(@PathVariable Long customerId) {
+
+    Customer customer = customerViewRepository.findOne(customerId);
+
+    if (customer == null) {
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    } else {
+        return new ResponseEntity<>(new GetCustomerResponse(customer.getId(), customer.getCreditLimit()), HttpStatus.OK);
+    }
   }
 
 
