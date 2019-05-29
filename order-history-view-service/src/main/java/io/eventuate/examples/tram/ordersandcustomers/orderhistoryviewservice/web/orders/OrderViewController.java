fd34ec62c3eb8fe9ab913be0f4382146955c4ae1
Poonam1708 <51155609+Poonam1708@users.noreply.github.com>
2019-05-29 16:25:09 +0530

    Update OrderViewController.java

--- order-history-view-service/src/main/java/io/eventuate/examples/tram/ordersandcustomers/orderhistoryviewservice/web/orders/OrderViewController.java
+++ order-history-view-service/src/main/java/io/eventuate/examples/tram/ordersandcustomers/orderhistoryviewservice/web/orders/OrderViewController.java
@@ -1,7 +1,5 @@
 package io.eventuate.examples.tram.ordersandcustomers.orderhistoryviewservice.web.orders;
 
-import io.eventuate.examples.tram.ordersandcustomers.orderhistory.backend.OrderViewRepository;
-import io.eventuate.examples.tram.ordersandcustomers.orderhistory.common.OrderView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,22 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OrderViewController {
 
-  private OrderViewRepository orderViewRepository;
+  // private OrderService orderService;
+  private OrderRepository orderViewRepository;
 
   @Autowired
-  public OrderViewController(OrderViewRepository orderViewRepository) {
-    this.orderViewRepository = orderViewRepository;
+  public OrderViewController(OrderRepository orderRepository) {
+        this.orderViewRepository = orderRepository;
   }
-
+ 
 
   @RequestMapping(value="/orders/{orderId}", method= RequestMethod.GET)
-  public ResponseEntity<OrderView> getOrder(@PathVariable Long orderId) {
+  public ResponseEntity<GetOrderResponse> getOrder(@PathVariable Long orderId) {
 
-    OrderView ov = orderViewRepository.findOne(orderId);
-    if (ov == null) {
-      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    Order order = orderViewRepository.findOne(orderId);
+
+    if (order == null) {
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     } else {
-      return new ResponseEntity<>(ov, HttpStatus.OK);
+        return new ResponseEntity<>(new GetOrderResponse(order.getId(), order.getOrderDetails()), HttpStatus.OK);
     }
   }
 
